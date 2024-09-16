ec253483e5c56f6ce8d057847e00d6c76e560bec
Ziadabdelraouf <ziadabdelraouff@gmail.com>
2024-09-16 22:09:38 +0300

    till section 116

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -20,7 +20,12 @@ class _NewExpenseState extends State<NewExpense> {
 //this controller is built in flutter that controlls all the inputs and store it and can later be used to print.
   final _titleController = TextEditingController();
   final _priceController = TextEditingController();
+
+//intialized and non intialized 
   DateTime? _selectedDate;
+  Category _selectedcategory = Category.leisure;
+
+  // gets rid of the conrollers(same as delete in c++)
   @override
   void dispose() {
     _titleController.dispose();
@@ -28,8 +33,9 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
-  void _datePicker()async {
-    final pickedDate= await showDatePicker(
+//async has to be used as showdatepicker return a value of future so we must use (async and await)
+  void _datePicker() async {
+    final pickedDate = await showDatePicker(
       context: context,
       firstDate: DateTime(
         DateTime.now().year - 1,
@@ -40,7 +46,7 @@ class _NewExpenseState extends State<NewExpense> {
       lastDate: DateTime.now(),
     );
     setState(() {
-      _selectedDate=pickedDate;
+      _selectedDate = pickedDate;
     });
   }
 
@@ -50,6 +56,7 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
+          //to input data(text) from user
           TextField(
             controller: _titleController,
             maxLength: 50,
@@ -58,6 +65,7 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             children: [
               Expanded(
+                //to input data (numbers) drom the user
                 child: TextField(
                   keyboardType: TextInputType.number,
                   controller: _priceController,
@@ -70,13 +78,17 @@ class _NewExpenseState extends State<NewExpense> {
               const SizedBox(
                 width: 16,
               ),
+              // used expanded to prevent problems when a row is inside a row and these thimgs
               Expanded(
                 child: Row(
+                  //push elemnts to the end of the row 
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                     Text(_selectedDate!=null?formatter.format(_selectedDate!):'Select Date'),
+                    Text(_selectedDate != null
+                        ? formatter.format(_selectedDate!)
+                        : 'Select Date'),
                     IconButton(
-                      onPressed:_datePicker,
+                      onPressed: _datePicker,
                       icon: const Icon(
                         Icons.calendar_month,
                       ),
@@ -86,8 +98,32 @@ class _NewExpenseState extends State<NewExpense> {
               )
             ],
           ),
+          const SizedBox(
+            height: 20,
+          ),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedcategory,
+                items: Category.values
+                    .map(
+                      //maps a list values to list of obejcts to display them in drop down menu
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedcategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
